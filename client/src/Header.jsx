e00035beaa23841d1001cb3f57ffedb1875c210e
ross <ross.russell@protonmail.com>
2021-05-19 19:26:55 -0700

    material test run

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button, ButtonGroup } from '@material-ui/core';
 
 const Header = () => {
+  const displayDesktop = () => <Toolbar>pixelVault</Toolbar>;
+
   return (
-    <div id="headerContainer">
-      <Link to="/pagetwo">Click for page two</Link>
-    </div>
+    <header>
+      <AppBar>
+        {displayDesktop()}
+        <ButtonGroup
+          variant="contained"
+          color="secondary"
+          aria-label="outlined primary button group"
+        >
+          <Button>
+            <Link to="/pagetwo">Click for page two</Link>
+          </Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+      </AppBar>
+    </header>
   );
 };
 
