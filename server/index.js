f529811a7ecc1209870c4f6e1bce961e62e47dbf
wolfshirts <ross.russell@protonmail.com>
2021-05-28 17:11:37 -0700

    no idea why favicon isn't loading

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 require('../database/connect.js');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
-
 app.use(express.static('public'));
 app.use('/assets', express.static('assets'));
 
