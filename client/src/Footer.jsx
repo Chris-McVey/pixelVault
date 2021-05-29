38f672c81fe51c5224639b84a09ad7d85e24e5bf
wolfshirts <ross.russell@protonmail.com>
2021-05-28 17:40:09 -0700

    worked on footer.

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -3,15 +3,31 @@ import { AppBar } from '@material-ui/core';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
-
-const Footer = () => (
-  <div>
-    {/* <AppBar position="fixed" color="primary" style={{ top: 'auto', bottom: 0 }}> */}
-    <AppBar position="sticky">
-      <YouTubeIcon />
-      <InstagramIcon />
-      <FacebookIcon />
-    </AppBar>
-  </div>
-);
+const footerClickHandler = (e) => {
+  console.log(e);
+};
+const Footer = () => {
+  const appBottomStyle = {
+    display: 'flex',
+    flexDirection: 'inherit',
+    justifyContent: 'space-evenly',
+    marginTop: '10px',
+  };
+  return (
+    <div>
+      {/* <AppBar position="fixed" color="primary" style={{ top: 'auto', bottom: 0 }}> */}
+      <AppBar style={appBottomStyle} position="sticky">
+        <div className="youtube-icon">
+          <YouTubeIcon onClick={footerClickHandler} fontSize="large" />
+        </div>
+        <div className="insta-icon">
+          <InstagramIcon onClick={footerClickHandler} fontSize="large" />
+        </div>
+        <div className="face-icon">
+          <FacebookIcon onClick={footerClickHandler} fontSize="large" />
+        </div>
+      </AppBar>
+    </div>
+  );
+};
 export default Footer;
