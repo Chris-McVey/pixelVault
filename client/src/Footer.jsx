77d59c100320bdc4c37ee973d26e640001239382
wolfshirts <ross.russell@protonmail.com>
2021-05-28 17:24:42 -0700

    added navbar stuck to bottom

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { AppBar } from '@material-ui/core';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
-const Footer = () => {
-  return (
-    <div>
-      <AppBar position="sticky">my AppBar</AppBar>
-    </div>
-  );
-  // return (
-  //   <div id="footerContainer">
-  //     <div>Footer Here</div>
-  //   </div>
-  // );
-};
-
+const Footer = () => (
+  <div>
+    <AppBar position="fixed" color="primary" style={{ top: 'auto', bottom: 0 }}>
+      <YouTubeIcon />
+      <InstagramIcon />
+      <FacebookIcon />
+    </AppBar>
+  </div>
+);
 export default Footer;
