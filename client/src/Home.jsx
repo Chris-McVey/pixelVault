ed9e86dd993d99697fcbce0471b397a2cba07ecb
Chris McVey <chris.mcvey@gmail.com>
2021-05-29 19:17:16 -0700

    Basic layout for home page and some other style changes. New font added

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,8 +3,50 @@ import { Container } from '@material-ui/core';
 
 const Home = () => {
   return (
-    <Container>
-      <div>Home Page</div>
+    <Container id="home-container">
+      <h1 id="home-heading">Welcome to Pixel Vault Games!</h1>
+      <img
+        id="games-in-case"
+        src="../assets/gamesInCase.jpeg"
+        alt="Store display case with NES games"
+      />
+      <p id="home-first-block-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum
+        porttitor risus, id fringilla ante viverra nec. Aliquam tortor lorem,
+        mollis quis ex ut, elementum mollis arcu. Nunc ac ligula luctus ante
+        eleifend molestie ac eget dolor. Integer ipsum neque, vehicula quis
+        tristique at, convallis id ante. Sed ac sapien eleifend, ornare urna
+        eget, lobortis mi. Cras enim tortor, interdum sit amet gravida nec,
+        ultrices ac magna.
+      </p>
+      <img
+        id="consoles-and-controllers"
+        src="../assets/consolesAndControllers.jpeg"
+        alt="Store display case with boxed consoles and controllers"
+      />
+      <p id="home-second-block-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum
+        porttitor risus, id fringilla ante viverra nec. Aliquam tortor lorem,
+        mollis quis ex ut, elementum mollis arcu. Nunc ac ligula luctus ante
+        eleifend molestie ac eget dolor. Integer ipsum neque, vehicula quis
+        tristique at, convallis id ante. Sed ac sapien eleifend, ornare urna
+        eget, lobortis mi. Cras enim tortor, interdum sit amet gravida nec,
+        ultrices ac magna.
+      </p>
+      <img
+        id="home-store-display"
+        src="../assets/storeDisplay2.jpeg"
+        alt="In-store display depicting scene with Mario, Link, and other video game characters"
+      />
+      <p id="home-third-block-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum
+        porttitor risus, id fringilla ante viverra nec. Aliquam tortor lorem,
+        mollis quis ex ut, elementum mollis arcu. Nunc ac ligula luctus ante
+        eleifend molestie ac eget dolor. Integer ipsum neque, vehicula quis
+        tristique at, convallis id ante. Sed ac sapien eleifend, ornare urna
+        eget, lobortis mi. Cras enim tortor, interdum sit amet gravida nec,
+        ultrices ac magna.
+      </p>
     </Container>
   );
 };
--- client/src/Instagram.jsx
+++ client/src/Instagram.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Instagram = () => {
   return (
-    <div id="curator-feed-default-feed-layout">
-      <a
-        href="https://curator.io"
-        target="_blank"
-        className="crt-logo crt-tag"
-      ></a>
+    <div>
+      <h2 id="instagram-heading">
+        Follow us on Instagram to be first to see what&apos;s in stock!
+      </h2>
+      <div id="curator-feed-default-feed-layout" />
     </div>
   );
 };
