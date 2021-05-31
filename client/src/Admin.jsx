3dc9aa9f6e211f5e5c91d74162cae69645d62f03
ross <ross.russell@protonmail.com>
2021-05-31 16:44:48 -0700

    forms work, just need to post to DB

--- client/src/Admin.jsx
+++ client/src/Admin.jsx
@@ -8,13 +8,36 @@ import {
   Button,
 } from '@material-ui/core';
 
-const handleNewsInputChange = (e) => {
-  console.log('input changed to: ', e.target.value);
-};
-
 const Admin = () => {
   const defaults = { newsTitle: '', newsBody: '' };
   const [formValues, setFormValues] = useState(defaults);
+  const handleNewsInputChange = (e) => {
+    if (e.target.name === 'news-title') {
+      setFormValues({
+        ...formValues,
+        newsTitle: e.target.value,
+      });
+    }
+    if (e.target.name === 'post-body') {
+      setFormValues({
+        ...formValues,
+        newsBody: e.target.value,
+      });
+    }
+  };
+  const handleNewsSubmit = () => {
+    if (formValues.newsBody === '' || formValues.newsTitle === '') {
+      return;
+    }
+    // post news to DB
+  };
+  const handleNewsClear = () => {
+    // document.getElementById('title').value = '';
+    // document.getElementById('post-body').value = '';
+    setFormValues({
+      ...defaults,
+    });
+  };
   console.log(formValues);
   return (
     <div className="admin-panel">
@@ -27,24 +50,38 @@ const Admin = () => {
           name="news-title"
           fullWidth
           id="title"
+          required
+          value={formValues.newsTitle}
           onChange={handleNewsInputChange}
         />
       </div>
       <div className="news-body">
         <TextField
-          required="true"
+          required
           label="Post Body"
           fullwidth
           multiline
           rowsMax={10}
           id="post-body"
           name="post-body"
+          value={formValues.newsBody}
+          onChange={handleNewsInputChange}
         />
         <div className="news-submit">
-          <Button variant="contained" color="primary" type="button">
+          <Button
+            variant="contained"
+            onClick={handleNewsSubmit}
+            color="primary"
+            type="button"
+          >
             Submit
           </Button>
-          <Button variant="contained" color="primary" type="button">
+          <Button
+            variant="contained"
+            onClick={handleNewsClear}
+            color="primary"
+            type="button"
+          >
             Cancel
           </Button>
         </div>
