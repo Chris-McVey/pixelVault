fa7a1b4378fad00a53deb02e6e272d0e6c644325
Chris McVey <chris.mcvey@gmail.com>
2021-05-30 19:19:51 -0700

    Embedded google map now responsive

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -27,8 +27,8 @@ const Contact = () => {
         <iframe
           title="map-iframe"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.2510873975575!2d-117.66847098442098!3d34.063077180603095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c33391f06159ef%3A0xfa790ae5d3d6002d!2sPixel%20Vault%20Games!5e0!3m2!1sen!2sus!4v1622244894157!5m2!1sen!2sus"
-          width="600"
-          height="450"
+          width="100%"
+          height="100%"
           style={styling}
           allowFullScreen=""
           loading="lazy"
