9e1bb0dfe5fbf472be53d612e56a40ad70bcddd9
Chris McVey <chris.mcvey@gmail.com>
2021-05-31 18:21:55 -0700

    News entries displaying date properly

--- client/src/NewsEntry.jsx
+++ client/src/NewsEntry.jsx
@@ -4,10 +4,13 @@ import PropTypes from 'prop-types';
 import { Container } from '@material-ui/core';
 
 const NewsEntry = ({ title, date, text }) => {
+  const [day, month, year] = new Date(date)
+    .toLocaleDateString('en-US')
+    .split('/');
   return (
     <Container maxWidth="md" className="news-entry">
       <h3 className="news-entry-title">{title}</h3>
-      <p className="news-entry-date">{date}</p>
+      <p className="news-entry-date">{`${day}-${month}-${year}`}</p>
       <p className="news-entry-Text">{text}</p>
     </Container>
   );
