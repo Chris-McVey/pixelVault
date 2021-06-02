6736318276df434b3d5f764817fe81292fc7ca19
Chris McVey <chris.mcvey@gmail.com>
2021-06-01 20:16:18 -0700

    News feed only rendering 5 posts. Button added. Someone please style it

--- client/src/NewsFeed.jsx
+++ client/src/NewsFeed.jsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
 import axios from 'axios';
 
 import NewsEntry from './NewsEntry.jsx';
 
 const NewsFeed = () => {
   const [newsList, setNewsList] = useState([]);
+  const [displayed, setDisplayed] = useState([]);
 
   useEffect(() => {
     axios
       .get('/api/news')
       .then((response) => {
         const posts = response.data;
+        posts.reverse();
+        const toBeDisplayed = posts.splice(0, 5);
         setNewsList(posts);
+        setDisplayed(toBeDisplayed);
       })
       .catch(() => {
         setNewsList([
@@ -25,14 +30,21 @@ const NewsFeed = () => {
       });
   }, []);
 
-  const chronologicalNews = newsList;
-  chronologicalNews.reverse();
-
   return (
     <div id="news-feed">
-      {chronologicalNews.map(({ _id, title, date, text }) => {
+      {displayed.map(({ _id, title, date, text }) => {
         return <NewsEntry key={_id} title={title} date={date} text={text} />;
       })}
+      {newsList.length ? (
+        <Button
+          variant="contained"
+          color="secondary"
+          aria-label="outlined primary button group"
+          id="news.feed.button"
+        >
+          More
+        </Button>
+      ) : null}
       <img
         id="game-over"
         src="../assets/gameOver.jpg"
