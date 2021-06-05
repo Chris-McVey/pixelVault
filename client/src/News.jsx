738ff108e0416b44b6f1db553ffcc5fc52200dfd
Chris McVey <chris.mcvey@gmail.com>
2021-06-04 20:56:33 -0700

    Fixed some visual inconsistencies in CSS

--- client/src/News.jsx
+++ client/src/News.jsx
@@ -5,10 +5,12 @@ import NewsFeed from './NewsFeed.jsx';
 
 const News = () => {
   return (
-    <Container id="news-container">
-      <h1 id="news-heading">News and Updates</h1>
-      <NewsFeed />
-    </Container>
+    <div id="news-outer-container">
+      <Container id="news-container">
+        <h1 id="news-heading">News and Updates</h1>
+        <NewsFeed />
+      </Container>
+    </div>
   );
 };
 
